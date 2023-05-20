8ddf3e203f1d740f9020cc1bde55b84bbad03915
tsg2k2 <tsg2k2@gmail.com>
2023-05-20 10:41:30 -0400

    experiments with rtl9301

--- target/linux/ipq807x/generic/target.mk
+++ target/linux/ipq807x/generic/target.mk
@@ -1,3 +1,3 @@
 BOARDNAME:=Generic
 
-DEFAULT_PACKAGES += ath11k-firmware-qcn9074 uboot-envtools e2fsprogs kmod-fs-ext4 losetup kmod-ath11k-pci mdio-tools ethtool-full aq-fw-download kmod-usb-storage kmod-usb-storage-uas block-mount libblkid
+DEFAULT_PACKAGES += uboot-envtools e2fsprogs kmod-fs-ext4 losetup kmod-ath11k-pci mdio-tools ethtool-full aq-fw-download kmod-usb-storage kmod-usb-storage-uas block-mount libblkid kmod-spi-dev spidev-test spi-tools
